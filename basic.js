e617e3bafbf75a05026a9ad0ce6123421d27210d
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2025-11-11 20:44:47 +0530

    javascript

---
+++ basic.js
@@ -0,0 +1,16 @@
+let a = Math.floor(Math.random() * 100);
+
+console.log("Marks: ", a);
+if (a > 90) {
+    console.log("O")
+} else if (a > 80) {
+    console.log("A+");
+} else if (a > 70) {
+    console.log("A");
+} else if (a > 60) {
+    console.log("B+");
+} else if (a > 50) {
+    console.log("B");
+} else if (a > 40) {
+    console.log("C");
+}
